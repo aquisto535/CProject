508381beb34fafc2789776c5338d4a543f15c6bf
Jung Tae Min <outopia3@gmail.com>
2023-05-09 22:01:01 +0900

    add win32

---
+++ ExamWin32_base_code (2)/ExamWin32/ExamWin32.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include "tipsware.h"
+
+NOT_USE_MESSAGE;
+
+int main()
+{
+
+
+    ShowDisplay(); // 정보를 윈도우에 출력한다.
+    return 0;
+}
---
+++ ExamWin32_base_code (2)/ExamWin32/pch.cpp
@@ -0,0 +1,11 @@
+﻿// pch.cpp: 미리 컴파일된 헤더에 해당하는 소스 파일
+
+#include "pch.h"
+
+/*
+// 응용 프로그램이 사용하는 내부 윈도우 클래스 이름입니다. (수정 가능)
+const char *gp_app_name = "ExamGameMacro";
+// 응용 프로그램이 사용하는 제목입니다. (수정 가능)
+const char *gp_window_title = "게임용 매크로 예제(Step 1, https://blog.naver.com/tipsware)";
+// 응용 프로그램이 사용할 메뉴의 ID입니다. (0이면 메뉴를 사용하지 않는다는 뜻입니다.)
+*/
