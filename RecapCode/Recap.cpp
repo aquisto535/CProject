cf3b18fe34a0d756ce0487f959a29ce1f6018a43
Jung Tae Min <71391013+aquisto535@users.noreply.github.com>
2025-05-21 22:09:25 +0900

    특정 비트의 값 얻는 방법

--- RecapCode/Recap.cpp
+++ RecapCode/Recap.cpp
@@ -1,31 +1,44 @@
 #include <stdio.h>
 
-unsigned char ResetBit(unsigned char dest_data, unsigned char bit_num)
+unsigned char GetBit(unsigned char dest_data, unsigned char bit_num)
 {
-	/* 1.0x01 << bit_num: 지정된 크기만큼 0x01을 비트 이동시킴(1바이트 변수라서 비트 이동은 1~7까지만 가능함) */
-	/* 2. OR 연산 진행 */
+	unsigned char bit_state = 0;
+	
+	if (bit_num < 8)
+	{
+		bit_state = dest_data & (0x01 << bit_num); //특정 비트의 값을 얻기 위해 다른 비트를 0으로 초기화
+		bit_state = bit_state >> bit_num; //특정 비트의 값(0 또는 1)을 확인하기 위해 해당 위치에서 가장 오른쪽 위치의 비트로 이동
 
-	if (bit_num < 8) dest_data = dest_data  | (0x01 << bit_num); 
+	}
 
 	//변경된 값 확인
 
-	return dest_data;
+	return bit_state;
 
 }
 
 int main()
 {
-	unsigned char lamp_state = 0x7F;
-	printf("%X -> ", lamp_state); // 변경 전 값 출력ㄴ 
+	unsigned char lamp_state = 0x75;
+	unsigned char bit_state;
+	int i;
 
-	//lamp_state 변수의 3번째 비트를 0으로 설정
-	lamp_state = ResetBit(lamp_state, 3);
-	printf("%X", lamp_state); // 변경 후 값 출력
+	printf("%X -> ", lamp_state); // 변경 전 값 출력
+
+	for ( i = 0; i < 8; i++)
+	{
+		bit_state = GetBit(lamp_state, 7-i);
+
+		printf("%d ", bit_state);
+	}
+
+	printf("\n");
 
 	return 0;
 }
 
-//16진수(표기) : 0xcc 1100 1100
-//16진수(표기) : 0x30 0011 0000
+//EX) 1010 0?01 와 같은 값이 있는 경우 ?의 값을 알아내는 방법
+// 1. ?의 위치에만 1을 AND연산 진행하고 나머지를 0으로 AND 연산하여 해당 위치의 비트만 값이 유지되도록 설정(나머지 비트는 모두 0으로 설정)
+// 2. ?의 값을 알아내기 위해 ?의 위치를 시프트 연산을 통해 가장 오른쪽으로 이동시킴 0000 000? (0000 0?00 >> 2 -> 0000 000?)
 
 
